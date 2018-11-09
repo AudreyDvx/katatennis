5b765b1bf34d7af885d311c8340f5d8e4de1d923
Audrey <audrey.devaux@etu.unilim.fr>
2018-11-09 17:12:53 +0100

    refactor

--- src/main/java/fr/unilim/iut/katatennis/TennisGame1.java
+++ src/main/java/fr/unilim/iut/katatennis/TennisGame1.java
@@ -3,8 +3,8 @@ package fr.unilim.iut.katatennis;
 
 public class TennisGame1 implements TennisGame {
     
-    private int m_score1 = 0;
-    private int m_score2 = 0;
+    private int mScore1 = 0;
+    private int mScore2 = 0;
     private String player1Name;
     private String player2Name;
 
@@ -15,17 +15,18 @@ public class TennisGame1 implements TennisGame {
 
     public void wonPoint(String playerName) {
         if (playerName == "player1")
-            m_score1 += 1;
+            mScore1 += 1;
         else
-            m_score2 += 1;
+            mScore2 += 1;
     }
 
     public String getScore() {
         String score = "";
         int tempScore=0;
-        if (m_score1==m_score2)
+        StringBuilder bld = new StringBuilder();
+        if (mScore1==mScore2)
         {
-            switch (m_score1)
+            switch (mScore1)
             {
                 case 0:
                         score = "Love-All";
@@ -42,9 +43,9 @@ public class TennisGame1 implements TennisGame {
                 
             }
         }
-        else if (m_score1>=4 || m_score2>=4)
+        else if (mScore1>=4 || mScore2>=4)
         {
-            int minusResult = m_score1-m_score2;
+            int minusResult = mScore1-mScore2;
             if (minusResult==1) score ="Advantage player1";
             else if (minusResult ==-1) score ="Advantage player2";
             else if (minusResult>=2) score = "Win for player1";
@@ -54,23 +55,29 @@ public class TennisGame1 implements TennisGame {
         {
             for (int i=1; i<3; i++)
             {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
+                if (i==1) tempScore = mScore1;
+                else { 
+                	bld.append("-");
+                	tempScore = mScore2;
+                }
                 switch(tempScore)
                 {
                     case 0:
-                        score+="Love";
+                    	bld.append("Love");
                         break;
                     case 1:
-                        score+="Fifteen";
+                    	bld.append("Fifteen");
                         break;
                     case 2:
-                        score+="Thirty";
+                    	bld.append("Thirty");
                         break;
                     case 3:
-                        score+="Forty";
+                    	bld.append("Forty");
                         break;
+                    default:
+                    	break;
                 }
+                score = bld.toString();
             }
         }
         return score;
