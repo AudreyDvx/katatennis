878e4be718d4d78563bcf3d5ec1743e9a01a781e
Audrey <audrey.devaux@etu.unilim.fr>
2018-11-09 17:19:23 +0100

    refactor des méthodes

--- src/main/java/fr/unilim/iut/katatennis/TennisGame1.java
+++ src/main/java/fr/unilim/iut/katatennis/TennisGame1.java
@@ -22,64 +22,76 @@ public class TennisGame1 implements TennisGame {
 
     public String getScore() {
         String score = "";
-        int tempScore=0;
         StringBuilder bld = new StringBuilder();
         if (mScore1==mScore2)
-        {
-            switch (mScore1)
-            {
-                case 0:
-                        score = "Love-All";
-                    break;
-                case 1:
-                        score = "Fifteen-All";
-                    break;
-                case 2:
-                        score = "Thirty-All";
-                    break;
-                default:
-                        score = "Deuce";
-                    break;
-                
-            }
-        }
+            score = egalite();
         else if (mScore1>=4 || mScore2>=4)
-        {
-            int minusResult = mScore1-mScore2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
-        }
+            score = avantages();
         else
-        {
-            for (int i=1; i<3; i++)
-            {
-                if (i==1) tempScore = mScore1;
-                else { 
-                	bld.append("-");
-                	tempScore = mScore2;
-                }
-                switch(tempScore)
-                {
-                    case 0:
-                    	bld.append("Love");
-                        break;
-                    case 1:
-                    	bld.append("Fifteen");
-                        break;
-                    case 2:
-                    	bld.append("Thirty");
-                        break;
-                    case 3:
-                    	bld.append("Forty");
-                        break;
-                    default:
-                    	break;
-                }
-                score = bld.toString();
-            }
-        }
+            score = gererScore(score, bld);
+        
         return score;
     }
+
+	private String gererScore(String score, StringBuilder bld) {
+        int tempScore=0;
+		for (int i=1; i<3; i++)
+		{
+		    if (i==1) tempScore = mScore1;
+		    else { 
+		    	bld.append("-");
+		    	tempScore = mScore2;
+		    }
+		    switch(tempScore)
+		    {
+		        case 0:
+		        	bld.append("Love");
+		            break;
+		        case 1:
+		        	bld.append("Fifteen");
+		            break;
+		        case 2:
+		        	bld.append("Thirty");
+		            break;
+		        case 3:
+		        	bld.append("Forty");
+		            break;
+		        default:
+		        	break;
+		    }
+		    score = bld.toString();
+		}
+		return score;
+	}
+
+	private String avantages() {
+		String score;
+		int minusResult = mScore1-mScore2;
+		if (minusResult==1) score ="Advantage player1";
+		else if (minusResult ==-1) score ="Advantage player2";
+		else if (minusResult>=2) score = "Win for player1";
+		else score ="Win for player2";
+		return score;
+	}
+
+	private String egalite() {
+		String score;
+		switch (mScore1)
+		{
+		    case 0:
+		            score = "Love-All";
+		        break;
+		    case 1:
+		            score = "Fifteen-All";
+		        break;
+		    case 2:
+		            score = "Thirty-All";
+		        break;
+		    default:
+		            score = "Deuce";
+		        break;
+		    
+		}
+		return score;
+	}
 }
